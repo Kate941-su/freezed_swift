00c9f937d0183b10b64a0b395514b163091d1b7f
Kaito Kitaya <kitaya@papyrusde.com>
2025-11-06 07:54:58 +0100

    fix: fix expanding bug when that contains computed properties and private fields

--- Sources/FreezedMacroClient/main.swift
+++ Sources/FreezedMacroClient/main.swift
@@ -2,13 +2,16 @@ import FreezedMacro
 import Foundation
 
 @Freezed
-final class User: Hashable {
+final class User {
     let id: Int
     let name: String
     let url: URL
     let optionalUrl: URL?
     let optionalName:String?
     let optionalId: Int?
+    private let a: Int = 1
+    private(set) var dummy: String
+    
     // マクロが依存する全プロパティを受け取るイニシャライザ
     // ※ 実際のマクロを使うにはこれが必須です。
     init(
@@ -25,7 +28,16 @@ final class User: Hashable {
         self.optionalUrl = optionalUrl
         self.optionalName = optionalName
         self.optionalId = optionalId
-    }    
+        dummy = "1"
+    }
+    
+    var nameDummy : String {
+        return "\(name)+Hello"
+    }
+    
+    var idDummy : Int {
+        return id + 1
+    }
     
 }
 
